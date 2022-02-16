a0b5638168904590c3761ec1bc3cbae2590c2b1f
S00ahKim <rlatndk0206@naver.com>
2022-02-16 23:58:35 +0900

    로그인 예외 처리, 첫번째 인기 게시물 좋아요

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from nis import cat
 from selenium import webdriver
 import inspect, os, platform, time
 from config import exec_options
@@ -31,15 +32,25 @@ def bot():
   print('로그인중....') 
   time.sleep(5)
 
-  # 2. 아이디 입력창을 찾아서 위에서 입력받은 아이디(insta_id)값 입력 
-  id_input = driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label') 
-  id_input.click() #입력창 클릭 
-  id_input.send_keys(insta_id) #아이디 입력 
+  # 2. 아이디 입력창을 찾아서 위에서 입력받은 아이디(insta_id)값 입력
+  try:
+    id_input = driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input') 
+    id_input.click() #입력창 클릭 
+    id_input.send_keys(insta_id) #아이디 입력 
+  except:
+    id_input = driver.find_element_by_xpath('/html/body/div[1]/div/div/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input') 
+    id_input.click() #입력창 클릭 
+    id_input.send_keys(insta_id) #아이디 입력 
   
   # 2-1. 패스워드 입력창을 찾아서 위에서 입력받은 패스워드(insta_pw)값 입력 
-  pw_input = driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label') 
-  pw_input.click() 
-  pw_input.send_keys(insta_pw) 
+  try:
+    pw_input = driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input') 
+    pw_input.click() 
+    pw_input.send_keys(insta_pw) 
+  except:
+    pw_input = driver.find_element_by_xpath('/html/body/div[1]/div/div/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input') 
+    pw_input.click() 
+    pw_input.send_keys(insta_pw) 
   
   # 3. 로그인 버튼 클릭 
   login_btn = driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button') 
@@ -58,30 +69,35 @@ def bot():
   alert_later.click()
   time.sleep(1)
 
-  # 5. 인기게시물 첫번째 피드 선택
-  first_feed = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[1]/div/div/div[1]/div[1]/a/div/div[2]') 
-  first_feed.click() 
-  time.sleep(1) 
-  
-  # 6. 좋아요 작업 - 입력한 횟수만큼 반복 작업 
-  for idx in range(insta_cnt): 
-    div = driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/article/div') 
-    div = div.find_element_by_xpath('/html/body/div[5]/div[2]/div/article/div[3]') 
-    like_btn = div.find_element_by_tag_name('button') #좋아요 버튼 
-    
-    like_btn.click() #좋아요 클릭 
-    print('{}번째 피드 좋아요 작업 완료'.format(idx + 1))
-    
-    # 너무 빠르게 작업을 할 경우 많은 양의 작업을 하게 되어 인스타그램측에서 계정 정지나 경고를 할 수 있으니
-    # 작업과 다음 작업 사이의 속도를 조절하기 위해 20초 이상을 설정해주세요. 
-    time.sleep(5)
+  # 검색어 입력
+  driver.get('https://www.instagram.com/explore/tags/' + insta_tag)
+  time.sleep(5)
 
-    # 7. 좋아요 작업 - 다음 피드로 이동 
-    if idx < insta_cnt: 
-      next_feed = driver.find_element_by_link_text('다음') 
-      next_feed.click()
+  # 5. 인기게시물 첫번째 피드 선택
+  first_feed = driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[1]/div/div/div[1]/div[1]/a') 
+  first_feed.click() 
+  time.sleep(1)
+  driver.find_element_by_xpath('/html/body/div[6]/div[3]/div/article/div/div[2]/div/div/div[2]/section[1]/span[1]/button').click()
   
-  print('모든 작업 완료') 
-  driver.quit()
+  # 6. 좋아요 작업 - 입력한 횟수만큼 반복 작업 (여기서부터 다음 누르면 될 듯) 
+  # for idx in range(insta_cnt): 
+  #   div = driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/article/div') 
+  #   div = div.find_element_by_xpath('/html/body/div[5]/div[2]/div/article/div[3]') 
+  #   like_btn = div.find_element_by_tag_name('button') #좋아요 버튼 
+    
+  #   like_btn.click() #좋아요 클릭 
+  #   print('{}번째 피드 좋아요 작업 완료'.format(idx + 1))
+    
+  #   # 너무 빠르게 작업을 할 경우 많은 양의 작업을 하게 되어 인스타그램측에서 계정 정지나 경고를 할 수 있으니
+  #   # 작업과 다음 작업 사이의 속도를 조절하기 위해 20초 이상을 설정해주세요. 
+  #   time.sleep(5)
+
+  #   # 7. 좋아요 작업 - 다음 피드로 이동 
+  #   if idx < insta_cnt: 
+  #     next_feed = driver.find_element_by_link_text('다음') 
+  #     next_feed.click()
+  
+  # print('모든 작업 완료') 
+  # driver.quit()
 
 bot()
